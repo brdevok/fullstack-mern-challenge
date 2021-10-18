16c00557ef577a62c8024e9a7fdcf1bef23a6db3
Braian Rosas <axis.dev@outlook.com.ar>
2021-10-18 13:06:30 -0300

    added settings

--- jest.config.ts
+++ jest.config.ts
@@ -14,16 +14,16 @@ export default {
   // cacheDirectory: "C:\\Users\\braia\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+  //clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false,
+  //collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  // coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
